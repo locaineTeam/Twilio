370dd325b90b435c3812425d1fa274335b1783bb
Simón Marín <german.marin@mail.escuelaing.edu.co>
2021-05-19 03:13:05 -0500

    Arreglando error monumental gracias a jauncho

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -3,13 +3,13 @@ import Chat from 'twilio-chat';
 import { Chat as ChatInterface } from '@progress/kendo-react-conversational-ui';
 
 class ChatApp extends Component {
-	constructor(props) {
-    //console.log(props);
+  constructor(props) {
+    
     super(props);
     this.state = {
       error: null,
       isLoading: true,
-      messages: []
+      messages:[]
     };
     this.room = props.room;
 
@@ -25,7 +25,37 @@ class ChatApp extends Component {
     this.messageAdded = this.messageAdded.bind(this);
     this.sendMessage = this.sendMessage.bind(this);
     this.handleError = this.handleError.bind(this);
+    this.mensajeAspero = null;
+    
+    
   }
+
+
+  async initializeIceBreaker(){
+    
+    
+    const text=await fetch(`https://ice2meetubackend.azurewebsites.net/getIceBreaker?language=${this.room}`);
+    const iceBreakerText = await text.json();
+    
+    
+    this.state.messages=[{text:iceBreakerText.iceBreakerBody,author:{id:null, name:"ICE BREAKER!"},timestamp:null}]
+  }
+
+
+  async fetchCommand(message, information, funcion){
+
+
+    const text=await fetch(`https://ice2meetubackend.azurewebsites.net/messages/command/${message}/language/${this.room}`);
+    const commandText = await text.json();
+    this.mensajeAspero = commandText.message;
+    funcion(commandText.message);
+    
+
+    
+
+    }
+
+  
 
   async componentDidMount() {
 
@@ -38,15 +68,16 @@ class ChatApp extends Component {
     
     }
 
-	handleError(error) {
-	    console.error(error);
-	    this.setState({
-	      error: 'Could not load chat.'
-	    });
-	  }
+  handleError(error) {
+      console.error(error);
+      this.setState({
+        error: 'Could not load chat.'
+      });
+    }
 
 
-	setupChatClient(client) {    
+  setupChatClient(client) {      
+    this.initializeIceBreaker();
     this.client = client;
     let nombre = this.room;
     this.client
@@ -68,74 +99,108 @@ class ChatApp extends Component {
         this.channel.on('messageAdded', this.messageAdded);
       })
       .catch(this.handleError);
-   	}
+    }
 
-   	twilioMessageToKendoMessage(message) {
-    return {
-      text: message.body,
-      author: { id: message.author, name: message.author },
-      timestamp: message.timestamp
-    	};
-	  }
+    twilioMessageToKendoMessage(message) {
+          
+      return {
 
-	messagesLoaded(messagePage) {
-      console.log(this);
-	    this.setState({
-	      messages: messagePage.items.map(this.twilioMessageToKendoMessage)
-	    	});
-	  }
+        text: message.body,
+        author: { id: message.author, name: message.author },
+        timestamp: message.timestamp
+        };
+    }
+    
 
-	  messageAdded(message) { 
+    
 
-    this.setState(prevState => (
+  messagesLoaded(messagePage) {
+      
+      this.setState({
+        messages: messagePage.items.map(this.twilioMessageToKendoMessage)
+        });
+    }
 
+    messageAdded(message) {
+
+    
+    if(message.body[0] === '#'){
+
+      const k = this.fetchCommand(message.body.slice(1),message,(m)=>{
+            console.log(m);
+            let variable = {body:m,author:this.user.id,timestamp:null};
+
+            this.setState(prevState => (
+
+              {
+         
+                messages: [
+                  ...prevState.messages,
+                  this.twilioMessageToKendoMessage(variable)
+                ]
+
+              }
+
+            ));
+            console.log(message);
+            //this.messageAdded(message);
+            
+
+      });
+
+
+
+    }
+    else{
+      this.setState(prevState => (
 
       {
-
-
-        
+ 
         messages: [
           ...prevState.messages,
           this.twilioMessageToKendoMessage(message)
         ]
 
-
       }
-
 
     ));
 
-    
-
+    }
 
   }
-	sendMessage(event) {
-	    this.channel.sendMessage(event.message.text);
-	}
+  sendMessage(event) {
 
-	componentWillUnmount() {
+      
+      
+      
+      this.channel.sendMessage(event.message.text);
+
+  }
+
+  componentWillUnmount() {
     this.client.shutdown();
   }
 
 
-	render() {
-	    if (this.state.error) {
-	      return <p>{this.state.error}</p>;
-	    } else if (this.state.isLoading) {
-	      return <p>Loading chat...</p>;
-	    }
-	    return (
+  render() {
+      
+      if (this.state.error) {
+        return <p>{this.state.error}</p>;
+      } else if (this.state.isLoading) {
+        return <p>Loading chat...</p>;
+      }
+      return (
         <div>
-  	      <ChatInterface
-  	        user={this.user}
-  	        messages={this.state.messages}
-  	        onMessageSend={this.sendMessage}
+          <ChatInterface
+            user={this.user}
+            messages={this.state.messages}
+            onMessageSend={this.sendMessage}
             placeholder={"Type a message..."}
-  	        width={500}
-  	      />
+            width={500}
+          />
         </div>  
-	    );
-	  }
+      );
+    }
 }
 
 export default ChatApp;
